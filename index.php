b4792cfdd30e103d06dfa697faaf88defcd37b09
Antonio Gage <antoniojgage@gmail.com>
2020-04-11 12:11:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
-echo 'My username is ' .$_ENV["USER"] . '!';
-?>
-<?php
 $myName = $_ENV["USER"];
+$lorisMiddleName = $_ENV["heyLori"];
 ?>
 <html>
+
 <head>
-<title>My Page</title>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <title>My Page</title>
 </head>
+
 <body>
-<h2>Your variable was <?php echo $myName; ?></h2>
+    <h2>Your variable was <?php echo $myName; ?></h2>
 </body>
+<script>
+    var lorisMiddleName = <?php
+                            $_ENV["heyLori"];
+                            ?>
+</script>
+<script>
+    $(document).ready(function() {
+        console.log("ready!");
+        console.log(lorisMiddleName)
+    });
+</script>
+
 </html>
